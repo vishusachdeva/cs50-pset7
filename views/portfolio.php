ebb36e61952b1cadef7d4525755e681546840c9f
Vinayak Sachdeva <vishusachdeva228@gmail.com>
2016-12-16 09:07:32 +0000

    The Next Day

--- views/portfolio.php
+++ views/portfolio.php
@@ -1,28 +1,26 @@
-<div id="middle">
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>Symbol</th>
-                <th>Name</th>
-                <th>Shares</th>
-                <th>Price</th>
-                <th>TOTAL</th>
-            </tr>
-        </thead>
-        <tbody>
-        <?php foreach ($positions as $position): ?>
-            <tr>
-                <td><?= $position["symbol"] ?></td>
-                <td><?= $position["name"] ?></td>
-                <td><?= $position["shares"] ?></td>
-                <td><?= $position["price"] ?></td>
-                <td><?= $position["total"]?></td>
-            </tr>
-        <?php endforeach ?>
-            <tr>
-                <td colspan="4"><?= "CASH" ?></td>
-                <td><?= "$".$cash ?></td>
-            </tr>
-        </tbody>
-    </table>
-</div>
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th>Symbol</th>
+            <th>Name</th>
+            <th>Shares</th>
+            <th>Price</th>
+            <th>TOTAL</th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($positions as $position): ?>
+        <tr>
+            <td><?= $position["symbol"] ?></td>
+            <td><?= $position["name"] ?></td>
+            <td><?= $position["shares"] ?></td>
+            <td><?= $position["price"] ?></td>
+            <td><?= $position["total"]?></td>
+        </tr>
+    <?php endforeach ?>
+        <tr>
+            <td colspan="4"><?= "CASH" ?></td>
+            <td><?= "$".$cash ?></td>
+        </tr>
+    </tbody>
+</table>
