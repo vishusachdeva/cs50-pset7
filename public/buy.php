da23ed994b70e607bb50b2513f425b176bdfd29c
Vinayak Sachdeva <vishusachdeva228@gmail.com>
2016-12-16 12:27:22 +0000

    completed sell now working on buy

--- public/buy.php
+++ public/buy.php
@@ -2,23 +2,12 @@
 
     // configuration
     require("../includes/config.php");
-    
+
     // if user reached page via GET (as by clicking a link or via redirect)
     if ($_SERVER["REQUEST_METHOD"] == "GET")
     {
-        $rows = CS50::query(
-            "SELECT symbol
-            FROM portfolios
-            WHERE user_id={$_SESSION["id"]}"
-        );
-    
-        if (count($rows) === 0)
-        {
-            apologize("Nothing to sell.");
-        }
-        
         // else render form
-        render("sell-form.php", ["title" => "Sell", "rows" => $rows]);
+        render("buy-form.php", ["title" => "Get Quote"]);
     }
 
     // else if user reached page via POST (as by submitting a form via POST)
@@ -27,30 +16,34 @@
         // validate submission
         if (empty($_POST["symbol"]))
         {
-            apologize("You must select a stock to sell.");
+            apologize("You must specify a stock to buy.");
+        }
+        else if (empty($_POST["shares"]))
+        {
+            apologize("You must specify a number of shares.");
         }
         else
         {
-            $rows = CS50::query(
-                "SELECT shares
-                FROM portfolios
-                where user_id={$_SESSION["id"]} AND symbol='{$_POST["symbol"]}'"
-            );
-            
-            if (count($rows) !== 1)
+            if (preg_match("/^\d+$/", $_POST["shares"]) == false)
             {
-                apologize("Server Error!! Please Try Later.");
+                apologize("Invalid number of shares.");
             }
             else
             {
                 $stock = lookup($_POST["symbol"]);
-                $cash_to_add = $stock["price"] * $rows[0]["shares"];
+                $cash_to_deduct = $stock["price"] * $_POST["shares"];
+                $check = CS50::query("SELECT cash FROM users WHERE id={$_SESSION["id"]});
+                if ($cash_to_deduct > $check[0]["cash"])
+                {
+                    apologize("You cant afford that.");
+                }
                 $del_upd = CS50::query(
                     "START TRANSACTION;
-                    DELETE FROM portfolios
-                    WHERE user_id = {$_SESSION["id"]} AND symbol = '{$_POST["symbol"]}';
+                    INSERT INTO portfolios (user_id, symbol, shares)
+                    VALUES({$_SESSION["id"]}, '{$_POST["symbol"]}', {$_POST["shares"]})
+                    ON DUPLICATE KEY UPDATE shares = shares + {$_POST["shares"]};
                     UPDATE users
-                    SET cash = cash + {$cash_to_add}
+                    SET cash = cash - {$cash_to_deduct}
                     WHERE id = {$_SESSION["id"]};
                     COMMIT;"
                 );
